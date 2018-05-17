f5469e48c33699f6b140c9ff7e540e6b14e090c4
Marat <maratatt@gmail.com>
2018-05-17 14:56:28 +0300

    Added Exception class

--- src/main/java/PuzzleGame.java
+++ src/main/java/PuzzleGame.java
@@ -5,21 +5,36 @@ import java.util.Iterator;
 public class PuzzleGame {
     String fileName;
     Parser parser;
-    ArrayList<String[]> puzzelPiecesInput = new ArrayList<String[]>();
+
 
     public PuzzleGame(String fileName) {
         this.fileName = fileName;
         parser = new Parser(fileName);
     }
 
-    public void startParser() throws IOException {
-        puzzelPiecesInput = parser.parse();
+    private ArrayList<String[]> startParser() throws IOException {
+
+        ArrayList<String[]>  puzzelPiecesInput = parser.parse();
+
+        // print for debug purpose only
         Iterator<String[]> ot = puzzelPiecesInput.iterator();
         while(ot.hasNext()){
             String[] tamp = ot.next();
             System.out.println(tamp[0]);
             System.out.println(tamp[1]);
         }
+
+        return puzzelPiecesInput;
     }
+
+    private void convertLinestoPieces(){};
+
+    private void validateBeforeSolver(){};
+
+    private void solver(){};
+
+
+
+
 
 }
