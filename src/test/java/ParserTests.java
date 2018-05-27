145fdf67e11eafb87fcbeff45fab575d4173a1a2
Yuli <yshlosbe@gmail.com>
2018-05-27 21:47:20 +0300

    update unit tests

--- src/test/java/ParserTests.java
+++ src/test/java/ParserTests.java
@@ -1,7 +1,9 @@
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -13,43 +15,127 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class ParserTests {
 
-    private static final String resourceDir= ("src/test/resources/filesToParse");
-    public static String[][] provideData(List<String> fileNamesFromFolder, String filepath) {
+private static final String resourceDir= ("src/test/resources/filesToParse/");
 
-        String[][] arObject = new String[fileNamesFromFolder.size()][];
-        int index = 0;
-        for (String obj : fileNamesFromFolder) {
-            arObject[index++] = new String[] { filepath, obj };
-        }
-        return arObject;
-    }
-
-    public static String[][] fileList(String diir) {
-        List<String> fileNames = new ArrayList<>();
-        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(resourceDir))) {
-            for (Path path : directoryStream) {
-                fileNames.add(path.toString());
-            }
-        } catch (IOException ex) {
-        }
-        return provideData(fileNames,resourceDir);
-    }
-
-    @ParameterizedTest
-    @MethodSource("stringProvider")
-    void testWithSimpleMethodSource(String[][] str) {
-
-        System.out.println(str[0][0]);
+    @Test
+    public void dashAfterPieceDetails() throws IOException {
+        Parser parse = new Parser(resourceDir + "dashAfterPieceDetails.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
 
     }
 
-    static Stream<Arguments> stringProvider() {
-        return Stream.of(
-                Arguments.of(fileList(resourceDir)));
+    @Test
+    public void elementsNumberEqualsMissing() throws IOException {
+        Parser parse = new Parser(resourceDir + "elementsNumberEqualsMissing.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
     }
+
+    @Test
+    public void emptyLines() throws IOException {
+        Parser parse = new Parser(resourceDir + "emptyLines.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void incorectPieceDetails() throws IOException {
+        Parser parse = new Parser(resourceDir + "incorectPieceDetails.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void lessElements() throws IOException {
+        Parser parse = new Parser(resourceDir + "lessElements.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void minusCharAppearTwice() throws IOException {
+        Parser parse = new Parser(resourceDir + "minusCharAppearTwice.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void missingElementsNum() throws IOException {
+        Parser parse = new Parser(resourceDir + "missingElementsNum.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void missingElementsNumString() throws IOException {
+        Parser parse = new Parser(resourceDir + "missingElementsNumString.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void missingHeader() throws IOException {
+        Parser parse = new Parser(resourceDir + "missingHeader.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void missingId() throws IOException {
+        Parser parse = new Parser(resourceDir + "missingId.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void missingPieceDetails() throws IOException {
+        Parser parse = new Parser(resourceDir + "missingPieceDetails.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void moreElements() throws IOException {
+        Parser parse = new Parser(resourceDir + "moreElements.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void sameId() throws IOException {
+        Parser parse = new Parser(resourceDir + "sameId.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+    @Test
+    public void whiteSpaces() throws IOException {
+        Parser parse = new Parser(resourceDir + "whiteSpaces.txt");
+        ArrayList<String[]> parseList = parse.parse();
+        assertTrue(parseList.size() > 0);
+
+    }
+
+
+
+
 }
 
 
